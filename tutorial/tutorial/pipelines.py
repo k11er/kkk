3899996d0de90952ead4d6a96c12bb8be2a0329a
k11er <k11er@github.com>
2018-01-05 17:42:08 +0800

    addtxt

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -5,12 +5,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+import os
+
 
 class TutorialPipeline(object):
-    def __init__(self):
-        self.file = open('items.json', 'w')
-
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
+        base_dir = os.getcwd()
+        fiename = base_dir + '/news.txt'
+        with open(fiename, 'a') as f:
+            f.write(str(item['date']))
+            f.write('\n')
+            f.write(str(item['tags']))
+            f.write('\n')
         return item
